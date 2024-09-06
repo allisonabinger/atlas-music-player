ac02f9aff1299a5cba4f86b3dc6d1ca9681916b1
Allison <allisonabinger@gmail.com>
2024-09-06 13:54:13 -0500

    A - 09/06 01:53 PM - Play Controls styling is complete with added functionality for buttons for playbackspeed, playstatus, and playbackmode

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -1,3 +1,14 @@
+// Music Player Component
+import CoverArt from "./components/CoverArt";
+import SongTitle from "./components/SongTitle";
+import PlayControls from "./components/PlayControls";
+
 export default function MusicPlayer() {
-  return <p>Music player goes here</p>;
+  return (
+    <div className="m-auto">
+      <CoverArt />
+      <SongTitle title="Tidal Drift" artist="Echoes of the Sea" />
+      <PlayControls />
+    </div>
+  );
 }
